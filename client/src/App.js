6067ffe45c4bb113796573645a90a24d0364a2ef
bpavlov <bnpavlov@gmail.com>
2023-12-07 13:47:06 +0200

    initial commit 2

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Fib from './Fib';
+import OtherPage from './OtherPage';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Docker</h1>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header> */}
+          <h3>Reactive App</h3>
+        <div>
+          <Link to="/">Home</Link>
+          <br></br>
+          <Link to="/otherpage">Other Page</Link>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+
+export default App;
